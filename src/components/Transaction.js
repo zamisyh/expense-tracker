89231b6f77233f65464c0f1562dc8cbf556d2411
zam-dev <zamsyh.work@gmail.com>
2022-05-07 10:15:01 +0700

    Using condition for showing transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 export const Transaction = ({transaction}) => {
-  return (
-    <>
-        <li className="minus">
-            {transaction.text} <span> {transaction.amount} </span><button className="delete-btn">x</button>
-        </li>
-    </>
-  )
+    const sign = transaction.amount < 0 ? '-' : '+' 
+
+
+    return (
+        <>
+            <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+                {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+            </li>
+        </>
+    )
 }
