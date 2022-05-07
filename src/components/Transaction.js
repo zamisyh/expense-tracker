60b0a6c80fea1b9b41a1d64ee69de2813382fcaa
zam-dev <zamsyh.work@gmail.com>
2022-05-07 10:29:52 +0700

    Showing current balance

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -7,7 +7,11 @@ export const Transaction = ({transaction}) => {
     return (
         <>
             <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-                {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+                {transaction.text} 
+                <span>
+                    {sign}${Math.abs(transaction.amount)}
+                </span>
+                <button className="delete-btn">x</button>
             </li>
         </>
     )
