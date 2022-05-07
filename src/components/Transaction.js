c53ae72ac58762dec0706075ee61d38914005f3a
zam-dev <zamsyh.work@gmail.com>
2022-05-07 10:42:22 +0700

    Showing income and expense

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 export const Transaction = ({transaction}) => {
     const sign = transaction.amount < 0 ? '-' : '+' 
 
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -7,7 +7,8 @@ const initialState = {
         { id: 1, text: 'Flower', amount: -20 },
         { id: 2, text: 'Salary', amount: 300 },
         { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
+        { id: 4, text: 'Camera', amount: 150 },
+        { id: 5, text: 'Laptop', amount: 500 }
     ]
 }
 
