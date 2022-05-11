02e54b735fd4b4daa4ede2e5643126b4eb676c7e
zam-dev <zamsyh.work@gmail.com>
2022-05-11 19:29:51 +0700

    Create Get and Add Transaction API

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -1,23 +1,57 @@
+const Transaction = require('../models/Transaction')
+
 // @desc Get all transactions
 // @route /api/v1/transactions
 // @access Public
 
-exports.getTransactions = (req, res, next) => {
-    res.send('GET transactions')
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transaction = await Transaction.find();
+        return res.status(200).json({
+            success: true,
+            count: transaction.length,
+            data: transaction
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        })
+    }
 }
 
 // @desc Add all transactions
 // @route /api/v1/transactions
 // @access Public
 
-exports.addTransactions = (req, res, next) => {
-    res.send('ADD transactions')
+exports.addTransactions = async (req, res, next) => {
+   try {
+        const { text, amount } = req.body
+        const transaction = await Transaction.create(req.body)
+        return res.status(201).json({
+            success: true,
+            data: transaction
+        })
+   } catch (error) {
+       if (error.name === 'ValidationError') {
+           const messages = Object.values(error.errors).map(val => val.message)
+           return res.status(400).json({
+               success: false,
+               error: messages
+           })
+       } else {
+            return res.status(500).json({
+                success: false,
+                error: 'Server Error'
+            })
+       }
+   }
 }
 
 // @desc Delete all transactions
 // @route /api/v1/transactions
 // @access Public
 
-exports.deleteTransactions = (req, res, next) => {
+exports.deleteTransactions = async (req, res, next) => {
     res.send('DELETE transactions')
 }
