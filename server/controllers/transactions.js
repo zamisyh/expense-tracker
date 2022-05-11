21d86113ab931e48982e516c5904378ab0af8fe7
zam-dev <zamsyh.work@gmail.com>
2022-05-11 19:40:15 +0700

    Create delete Transaction API

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -53,5 +53,17 @@ exports.addTransactions = async (req, res, next) => {
 // @access Public
 
 exports.deleteTransactions = async (req, res, next) => {
-    res.send('DELETE transactions')
+    try {
+        const transaction = await Transaction.findByIdAndDelete(req.params.id)
+        return res.status(200).json({
+            success: true,
+            message: 'Successfully deleted',
+            data: transaction
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        })
+    }
 }
