46a8aeb54c0f4ce9b33b2a7fe19a7905acd77d52
zam-dev <zamsyh.work@gmail.com>
2022-05-12 19:23:56 +0700

    Call api get transaction in client

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,15 +1,12 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from "../reducer/AppReducer";
+import axios from 'axios'
 
 //Initial state
 const initialState = {
-    transactions: [
-        // { id: 1, text: 'Flower', amount: -20 },
-        // { id: 2, text: 'Salary', amount: 300 },
-        // { id: 3, text: 'Book', amount: -10 },
-        // { id: 4, text: 'Camera', amount: 150 },
-        // { id: 5, text: 'Laptop', amount: 500 }
-    ]
+    transactions: [],
+    error: null,
+    loading: true
 }
 
 //Create context
@@ -18,6 +15,21 @@ export const GlobalContext = createContext(initialState)
 //Provider components
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    async function getTransactions(){
+        try{
+            const res = await axios.get("/api/v1/transactions")
+            dispatch({
+                type: "GET_TRANSACTIONS",
+                payload: res.data.data
+            })
+        }catch(err){
+            dispatch({
+                type: "TRANSACTION_ERROR",
+                payload: err.response.data.error
+            })
+        }
+    }
 
     // Handle Function
     const deleteTransaction = (id) => {
