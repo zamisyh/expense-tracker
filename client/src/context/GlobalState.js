c5094b455be6458af43388341dfe8fc9daccc9a0
zam-dev <zamsyh.work@gmail.com>
2022-05-12 19:59:24 +0700

    Create add transaction client from api server

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -48,11 +48,26 @@ export const GlobalProvider = ({ children }) => {
 
     }
 
-    const addTransaction = (transaction) => {
-        dispatch({
-            type: 'ADD_TRANSACTION',
-            payload: transaction
-        })
+    const addTransaction = async (transaction) => {
+        const config = {
+            headers:{
+                'Content-Type': 'application/json' 
+            }
+        }
+
+        try {
+            const res = await axios.post('/api/v1/transactions', transaction, config)
+            dispatch({
+                type: 'ADD_TRANSACTION',
+                payload: res.data.data
+            })
+        } catch (error) {
+            dispatch({
+                type: "TRANSACTION_ERROR",
+                payload: error.response.data.error
+            })
+        }
+
     }
 
 
